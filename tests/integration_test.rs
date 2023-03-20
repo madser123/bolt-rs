6fa229102998a2aba3691486cf9d1de8a0419c85
Mads Jensen <madsaj10@outlook.com>
2023-03-20 22:50:18 +0100

    Cleanup after merge

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,38 +1,6 @@
-use std::env::var;
 
-use bolt_rs as slack;
-use slack::{
-    block::{self, Blocks, AsBlock, AsBlocks},
-    element::{self, AsElement, AsElements},
-    comp::{self, Text},
-    message::Message,
-};
 
-const TEST_CHANNEL: &str = "CHANNEL";
-
-#[test]
-#[should_panic]
-fn section_too_many_fields_failure() {
-    let mut blocks = Blocks::new();
-
-    let result = blocks.push(block::Section::new()
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("*Bold text*").into())
-        .field(Text::mrkdwn("1 too many fields :))").into())
-    );
-
-    println!("{result:?}");
-    result.unwrap();
-}
-
-fn create_app() {
-    let bot_token = var("BOT_TOKEN").unwrap();
+#[tokio::test]
+async fn create_app() {
+    todo!()
 }
